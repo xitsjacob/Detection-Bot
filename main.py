5253e0473bcae4395bc201edb54aa9676cc799c5
xItsJacob <jacobpears78@gmail.com>
2024-05-01 18:09:11 -0500

    tracking last update errors

--- main.py
+++ main.py
@@ -11,21 +11,9 @@ import aiohttp
 load_dotenv(".env")
 
 auth_token = os.getenv("RBLX_TOKEN")
+rblx_id = (os.getenv("HHP_RBLX"), os.getenv("ICY_RBLX"))
 
-def check_rblx_past(user_id, presence_id):
-    last_status_hhp = 0
-    last_status_icy = 1
-
-    if user_id == os.getenv("HHP_RBLX") and presence_id != last_status_hhp:
-        last_status_hhp = presence_id
-        return True
-    elif user_id == os.getenv("ICY_RBLX") and presence_id != last_status_icy:
-        last_status_icy = presence_id
-        return True
-    else:
-        return False
-
-def get_rblx_presence(user_id):
+async def get_rblx_presence(user_id):
     headers = {
         "Authorization": f"Bearer {auth_token}"
     }
@@ -41,42 +29,68 @@ def get_rblx_presence(user_id):
         data = response.json()
         if len(data["userPresences"]) > 0:
             presence = data["userPresences"][0]["userPresenceType"]
-            if check_rblx_past(user_id, presence):
-                if presence == 1:
-                    return "On Website"
-                elif presence == 2:
-                    return "In-game"
-                else:
-                    return "Offline"
+            return presence
         else:
             return "User not found"
     except requests.exceptions.RequestException as e:
         return f"Error fetching data: {str(e)}"
 
+async def rblx_update_check(user_id):
+    last_rblx_hhp = 0
+    last_rblx_icy = 2
+
+    print(f"{last_rblx_hhp} | {last_rblx_icy}")
+
+    try:
+        presence_id = await get_rblx_presence(user_id)
+
+        if user_id == rblx_id[0] and presence_id != last_rblx_hhp:
+            last_rblx_hhp = presence_id
+            print(last_rblx_hhp)
+            if presence_id == 1:
+                return "On Website"
+            elif presence_id == 2:
+                return "In-game"
+            else:
+                return "Offline"
+        elif user_id == rblx_id[1] and presence_id != last_rblx_icy:
+            last_rblx_icy = int(presence_id)
+            print(last_rblx_icy)
+            if presence_id == 1:
+                return "On Website"
+            elif presence_id == 2:
+                return "In-game"
+            else:
+                return "Offline"
+    except Exception as e:
+        return print(f"Error: {e}")
+
+    
+
 async def track_rblx_status():
     updates = 1
 
     while True:
-        rblx_id = (os.getenv("HHP_RBLX"), os.getenv("ICY_RBLX"))
 
         for user_id in rblx_id:
-            rblx_status = get_rblx_presence(user_id)
+            rblx_status = await rblx_update_check(user_id)
 
             try:
-                async with aiohttp.ClientSession() as session:
-                    webhook = Webhook.from_url(os.getenv("RBLX_WEBHOOK_URL"), session=session)
-                    
-                    username = f"GeorgeGodsent ({user_id})" if user_id == os.getenv("HHP_RBLX") else f"IcyQueenbee ({user_id})"
-                    embed = discord.Embed(title=f"{username} | {rblx_status}")
-                    await webhook.send(embed=embed, username="RBLX Detection")
+                if rblx_status != None:
+                    async with aiohttp.ClientSession() as session:
+                        webhook = Webhook.from_url(os.getenv("RBLX_WEBHOOK_URL"), session=session)
+                        
+                        username = f"GeorgeGodsent ({user_id})" if user_id == os.getenv("HHP_RBLX") else f"IcyQueenbee ({user_id})"
+                        embed = discord.Embed(title=f"{username} | {rblx_status}")
+                        await webhook.send(embed=embed, username="RBLX Detection")
 
-                    print(f"RBLX message #{updates} sent successfully!")
-                    updates += 1
+                print(f"RBLX Update Check #{updates} successfully!")
+                updates += 1
 
             except Exception as e:
                 print(f"RBLX Error sending message: {e}")
 
-        time.sleep(120) # Check status every minute
+        time.sleep(60) # Check status every minute
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
