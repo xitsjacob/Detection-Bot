020bea736987a1de4f48b8fdb3466a8cafadbe69
xItsJacob <jacobpears78@gmail.com>
2024-05-01 17:15:09 -0500

    added components

--- main.py
+++ main.py
@@ -12,6 +12,19 @@ load_dotenv(".env")
 
 auth_token = os.getenv("RBLX_TOKEN")
 
+def check_rblx_past(user_id, presence_id):
+    last_status_hhp = 0
+    last_status_icy = 1
+
+    if user_id == os.getenv("HHP_RBLX") and presence_id != last_status_hhp:
+        last_status_hhp = presence_id
+        return True
+    elif user_id == os.getenv("ICY_RBLX") and presence_id != last_status_icy:
+        last_status_icy = presence_id
+        return True
+    else:
+        return False
+
 def get_rblx_presence(user_id):
     headers = {
         "Authorization": f"Bearer {auth_token}"
@@ -28,10 +41,13 @@ def get_rblx_presence(user_id):
         data = response.json()
         if len(data["userPresences"]) > 0:
             presence = data["userPresences"][0]["userPresenceType"]
-            if presence == 1 or presence == 2:
-                return "Online"
-            else:
-                return "Offline"
+            if check_rblx_past(user_id, presence):
+                if presence == 1:
+                    return "On Website"
+                elif presence == 2:
+                    return "In-game"
+                else:
+                    return "Offline"
         else:
             return "User not found"
     except requests.exceptions.RequestException as e:
@@ -41,16 +57,16 @@ async def track_rblx_status():
     updates = 1
 
     while True:
-        rblx_id = (os.environ("HHP_RBLX"), os.environ("ICY_RBLX"))
+        rblx_id = (os.getenv("HHP_RBLX"), os.getenv("ICY_RBLX"))
 
         for user_id in rblx_id:
             rblx_status = get_rblx_presence(user_id)
 
             try:
                 async with aiohttp.ClientSession() as session:
-                    webhook = Webhook.from_url(os.environ("RBLX_WEBHOOK_URL"), session=session)
+                    webhook = Webhook.from_url(os.getenv("RBLX_WEBHOOK_URL"), session=session)
                     
-                    username = f"GeorgeGodsent ({user_id})" if user_id == os.environ("HHP_RBLX") else f"IcyQueenbee ({user_id})"
+                    username = f"GeorgeGodsent ({user_id})" if user_id == os.getenv("HHP_RBLX") else f"IcyQueenbee ({user_id})"
                     embed = discord.Embed(title=f"{username} | {rblx_status}")
                     await webhook.send(embed=embed, username="RBLX Detection")
 
